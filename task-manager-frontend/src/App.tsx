95508cb2226588fdb7d3581f30bfdf8db0533e9e
nomaanhusain <nomaanhusain@gmail.com>
2025-05-29 12:53:34 +0200

    logo added

--- task-manager-frontend/src/App.tsx
+++ task-manager-frontend/src/App.tsx
@@ -1,9 +1,262 @@
 import { useState } from "react";
-import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator, useTheme, View, Image, Heading, useAuthenticator } from "@aws-amplify/ui-react";
+import { Text as AWSText, Button as AWSButton } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import TaskList from "./components/TaskList";
-import { Button, Box, Text, Flex } from "@chakra-ui/react"
+import { Button, Box, Text, Flex, } from "@chakra-ui/react"
 
+
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          alt="Tasks logo"
+          src="./tasks_bg.png"
+          width={150}
+          height={150}
+        />
+      </View>
+    );
+  },
+
+  Footer() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <AWSText color={tokens.colors.neutral[80]}>
+          <strong>Tasks</strong> is a simple task management application built with
+          <strong> AWS services </strong> and <strong>React</strong>.
+        </AWSText>
+      </View>
+    );
+  },
+
+  SignIn: {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Sign in to your account
+        </Heading>
+      );
+    },
+    Footer() {
+      const { toForgotPassword } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <AWSButton
+            fontWeight="normal"
+            onClick={toForgotPassword}
+            size="small"
+            variation="link"
+          >
+            Reset Password
+          </AWSButton>
+        </View>
+      );
+    },
+  },
+
+  SignUp: {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Create a new account
+        </Heading>
+      );
+    },
+    Footer() {
+      const { toSignIn } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <AWSButton
+            fontWeight="normal"
+            onClick={toSignIn}
+            size="small"
+            variation="link"
+          >
+            Back to Sign In
+          </AWSButton>
+        </View>
+      );
+    },
+  },
+  ConfirmSignUp: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  SetupTotp: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <AWSText>Footer Information</AWSText>;
+    },
+  },
+  ConfirmSignIn: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <AWSText>Footer Information</AWSText>;
+    },
+  },
+  ForgotPassword: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  ConfirmResetPassword: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  SelectMfaType: {
+    Header() {
+      return <Heading level={3}>Select Desired MFA Type</Heading>;
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  SetupEmail: {
+    Header() {
+      return <Heading level={3}>Email MFA Setup</Heading>;
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+};
+
+const formFields = {
+  signIn: {
+    username: {
+      placeholder: 'Enter your email',
+    },
+  },
+  signUp: {
+    password: {
+      label: 'Password:',
+      placeholder: 'Enter your Password:',
+      isRequired: false,
+      order: 2,
+    },
+    confirm_password: {
+      label: 'Confirm Password:',
+      order: 1,
+    },
+  },
+  forceNewPassword: {
+    password: {
+      placeholder: 'Enter your Password:',
+    },
+  },
+  forgotPassword: {
+    username: {
+      placeholder: 'Enter your email:',
+    },
+  },
+  confirmResetPassword: {
+    confirmation_code: {
+      placeholder: 'Enter your Confirmation Code:',
+      label: 'New Label',
+      isRequired: false,
+    },
+    confirm_password: {
+      placeholder: 'Enter your Password Please:',
+    },
+  },
+  setupTotp: {
+    QR: {
+      totpIssuer: 'test issuer',
+      totpUsername: 'amplify_qr_test_user',
+    },
+    confirmation_code: {
+      label: 'New Label',
+      placeholder: 'Enter your Confirmation Code:',
+      isRequired: false,
+    },
+  },
+  confirmSignIn: {
+    confirmation_code: {
+      label: 'New Label',
+      placeholder: 'Enter your Confirmation Code:',
+      isRequired: false,
+    },
+  },
+  setupEmail: {
+    email: {
+      label: 'New Label',
+      placeholder: 'Please enter your Email:',
+    },
+  },
+};
 
 function App() {
   const [refreshFlag, setRefreshFlag] = useState(0);
@@ -12,7 +265,7 @@ function App() {
   };
 
   return (
-    <Authenticator>
+    <Authenticator formFields={formFields} components={components}>
       {({ signOut, user}) => (
         <Box minH="100vh">
           {/* Top Navbar */}
@@ -24,9 +277,17 @@ function App() {
             align="center"
             justify="space-between"
           >
-            <Text fontSize="lg" fontWeight="bold">
-              Hello, {user?.signInDetails?.loginId}
-            </Text>
+          <Flex align="center" gap={4}>
+            <Image
+              src="./tasks_bg.png"
+              alt="Tasks Logo"
+              width={50}
+              height={50}
+            />
+              <Text fontSize="lg" fontWeight="bold">
+                Hello, {user?.signInDetails?.loginId}
+              </Text>
+            </Flex>
             <Button colorScheme="red" onClick={signOut}>
               Sign Out
             </Button>
